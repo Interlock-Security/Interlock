dd4a8f51427b9086cbb12e7701e98b02e5d28771
CrocAI <monytiktok001@gmail.com>
2023-05-03 17:51:21 +0300

    Add webinix_set_kiosk function to webinix.hpp header

--- include/webinix.hpp
+++ include/webinix.hpp
@@ -51,6 +51,11 @@ namespace webinix {
 		webinix_close(window);
 	}
 
+	// Enable/Disable Kiosk Mode (Default=False)
+	void set_kiosk(void* window, bool kiosk) {
+		webinix_set_kiosk(window, kiosk);
+	}
+
 	// Close all opened windows. wait() will break.
 	void exit(void) {
 		webinix_exit();
