b22a28582a6e2c026c0be659c0a9e8b2601bdd8e
CrocAI <monytiktok001@gmail.com>
2023-09-02 11:07:58 -0400

    Adding HTML UTF8 Tag
    - To allow Unicode test in the UI we should make sure `<meta charset="UTF-8">` is added to the HTML head tags

--- examples/C/call_c_from_js/main.c
+++ examples/C/call_c_from_js/main.c
@@ -76,6 +76,7 @@ int main() {
     const char* my_html = "<!DOCTYPE html>"
     "<html>"
     "  <head>"
+    "    <meta charset=\"UTF-8\">"
     "    <script src=\"webinix.js\"></script>"
     "    <title>Call C from JavaScript Example</title>"
     "    <style>"
