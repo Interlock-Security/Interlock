5f79f19faad355466bc50a52954ab212a6c00a58
CrocAI <monytiktok001@gmail.com>
2023-10-08 01:48:44 +0200

    Format C code

--- examples/C/minimal/main.c
+++ examples/C/minimal/main.c
@@ -4,14 +4,12 @@
 
 int main() {
 
-    size_t my_window = webinix_new_window();
-    webinix_show(my_window, "<html><head><script src=\"webinix.js\"></script></head> Hello World ! </html>");
-    webinix_wait();
-    return 0;
+	size_t my_window = webinix_new_window();
+	webinix_show(my_window, "<html><head><script src=\"webinix.js\"></script></head> Hello World ! </html>");
+	webinix_wait();
+	return 0;
 }
 
 #if defined(_MSC_VER)
-    int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow) {
-        return main();
-    }
+int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow) { return main(); }
 #endif
