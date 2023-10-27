ffa2d66181305911979c231447b427c17d37abaa
CrocAI <monytiktok001@gmail.com>
2023-10-27 11:58:26 -0400

    GNU Makefile Adding Musl-GCC
    - Adding musl-gcc
    - Renaming text-editor to main. It should be the same as other examples

---
+++ examples/C/text-editor/main.c
@@ -0,0 +1,38 @@
+// Text Editor in C using Webinix
+
+#include "webinix.h"
+
+void Close(webinix_event_t* e) {
+	printf("Exit.\n");
+
+	// Close all opened windows
+	webinix_exit();
+}
+
+int main() {
+
+	// Create a new window
+	int MainWindow = webinix_new_window();
+
+	// Set the root folder for the UI
+	webinix_set_root_folder(MainWindow, "ui");
+
+	// Bind HTML elements with the specified ID to C functions
+	webinix_bind(MainWindow, "__close-btn", Close);
+
+	// Show the window, preferably in a chromium based browser
+	if (!webinix_show_browser(MainWindow, "index.html", ChromiumBased))
+		webinix_show(MainWindow, "index.html");
+
+	// Wait until all windows get closed
+	webinix_wait();
+
+	// Free all memory resources (Optional)
+	webinix_clean();
+
+	return 0;
+}
+
+#if defined(_MSC_VER)
+int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow) { return main(); }
+#endif
